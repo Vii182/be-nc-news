34c3915533afc3784bd9f3964e67c3e26f6f41e9
Matt <bmatt0835@gmail.com>
2024-10-14 21:19:14 +0100

    Added model and controller for '/api/articles' endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,6 +1,19 @@
 const fs = require("fs/promises");
 const db = require("../db/connection");
 
+const fetchArticles = () => {
+    return db.query(`
+        SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, 
+        articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;
+        `)
+        .then((result) => {
+        return result.rows;
+    });
+}
+
 const fetchArticleById = (article_id) => {
     return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
     .then((result) => {
@@ -11,4 +24,4 @@ const fetchArticleById = (article_id) => {
     })
 }
 
-module.exports = { fetchArticleById }
+module.exports = { fetchArticles, fetchArticleById }
