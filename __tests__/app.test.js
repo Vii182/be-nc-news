dcdead51dc66ba8502357b8deaef8bf1f425f268
Matt <bmatt0835@gmail.com>
2024-10-14 12:33:44 +0100

    Created app test file and tests for /api/topics endpoint

---
+++ __tests__/app.test.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+
+
+beforeEach(() => {
+    return seed(data)
+});
+
+afterAll(() => {
+    return db.end()
+});
+
+
+describe("/api/topics", () => {
+    test("GET: 200 sends an array of all topics to the client in correct formats", () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((response) => {
+            expect(Array.isArray(response.body.topics)).toBe(true);
+            response.body.topics.forEach((topic) => {
+                expect(topic).toHaveProperty('slug');
+                expect(topic).toHaveProperty('description');
+                expect(typeof topic.slug).toBe('string');
+                expect(typeof topic.description).toBe('string');
+              });
+        })
+    })
+})
