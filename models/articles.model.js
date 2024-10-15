4337227a639572c9b7ce0681948f74a1ca16f3d2
Matt <bmatt0835@gmail.com>
2024-10-15 12:05:18 +0100

    Added model and controller for '/api/articles/:article_id/comments' endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -24,4 +24,11 @@ const fetchArticleById = (article_id) => {
     })
 }
 
-module.exports = { fetchArticles, fetchArticleById }
+const fetchCommentsByArticleId = (article_id) => {
+    return db.query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`, [article_id])
+    .then((result) => {
+        return result.rows;
+    })
+}
+
+module.exports = { fetchArticles, fetchArticleById, fetchCommentsByArticleId }
