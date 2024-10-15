b690ee2d480c30b28dad7b5797d559c3f24c8602
Matt <bmatt0835@gmail.com>
2024-10-15 12:06:49 +0100

    added handling for 'article_id/comments' endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticleById, getArticles } = require("./controllers/articles.controller");
+const { getArticleById, getArticles, getCommentsByArticleId } = require("./controllers/articles.controller");
 const { getApiEndpoints } = require("./controllers/api.controller");
 const { handleCustomErrors, handle400Errors, handle404Errors, handle500Errors } = require("./error-handling");
 const endpoints = require("./endpoints.json");
@@ -13,6 +13,8 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 
 app.get('/api/articles/:article_id', getArticleById);
+
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.use(handleCustomErrors);
 
