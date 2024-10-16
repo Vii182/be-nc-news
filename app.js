2b092b818eb39aab3b5c5514720d2682d2271a22
Matt <bmatt0835@gmail.com>
2024-10-16 10:37:16 +0100

    Added handling for PATCH /api/articles/:article_id endpoint, included new endpoint description in json

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticleById, getArticles, getCommentsByArticleId } = require("./controllers/articles.controller");
+const { getArticleById, getArticles, getCommentsByArticleId, patchArticleVotes } = require("./controllers/articles.controller");
 const { getApiEndpoints } = require("./controllers/api.controller");
 const { handleCustomErrors, handle400Errors, handle404Errors, handle500Errors } = require("./error-handling");
 const { postCommentByArticleId } = require("./controllers/comments.controller");
 
 app.use(express.json());
 
+//ROUTES
 app.get("/api", getApiEndpoints);
-
 app.get("/api/topics", getTopics);
-
 app.get("/api/articles", getArticles);
-
-app.get('/api/articles/:article_id', getArticleById);
-
+app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+app.patch("/api/articles/:article_id", patchArticleVotes);
 
-app.post("/api/articles/:article_id/comments", postCommentByArticleId)
-
+//ERROR HANDLING
 app.use(handleCustomErrors);
-
 app.use(handle400Errors);
-
 app.use(handle500Errors);
-
 app.use(handle404Errors);
+
 module.exports = app;
