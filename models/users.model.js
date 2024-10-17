8ba3c7c2f29914d3ff78176c1964cb6e1076ebd8
Matt <bmatt0835@gmail.com>
2024-10-17 11:15:50 +0100

    Removed unneccessary IF statement from users model

--- models/users.model.js
+++ models/users.model.js
@@ -5,9 +5,6 @@ const db = require("../db/connection")
 const fetchUsers = () => {
     const query = `SELECT * FROM users;`
     return db.query(query).then((result) => {
-        if (result.rows.length === 0){
-          return Promise.reject({ status: 404, msg: 'No Users found'})
-        }
           return result.rows;
         });
 }
