bdf98222541c0944763100fc345959a43106d2ed
Matt <bmatt0835@gmail.com>
2024-10-20 22:01:14 +0100

    Added controller and model for new endpoint '/api/users/:username', updated users router

--- models/users.model.js
+++ models/users.model.js
@@ -9,5 +9,15 @@ const fetchUsers = () => {
         });
 }
 
+const fetchUserByUsername = (username) => {
+  return db.query(`SELECT * FROM users WHERE username = $1;`, [username])
+  .then((result) => {
+    if (result.rows.length === 0){
+      return Promise.reject({ status: 404, msg: 'No user found'})
+    }
+    return result.rows[0];
+  })
+}
 
-module.exports = { fetchUsers };
+
+module.exports = { fetchUsers, fetchUserByUsername };
